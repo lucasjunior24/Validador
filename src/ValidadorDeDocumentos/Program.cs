cc3d8a27253414becdfd2dac13456453b1d02ec0
lucas souza <lukateku@gmail.com>
2021-09-24 22:51:48 -0400

    Adicionar arquivos de projeto.

---
+++ src/ValidadorDeDocumentos/Program.cs
@@ -0,0 +1,33 @@
+﻿using Caelum.Stella.CSharp.Validation;
+using System;
+using System.Diagnostics;
+
+namespace ValidadorDeDocumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf1 = "03444081243";
+            string cpf2 = "04444444434";
+            string cpf3 = "44444444444";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine("CPF Válido: " + cpf);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("CPF Inválido! " + cpf + " : " + exc.Message);
+            }
+        }
+    }
+}
